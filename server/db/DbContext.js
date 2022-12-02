237871dff477f32d0f3182ca00e8e0072126a896
Derek Hearst <derekallenhearst@gmail.com>
2022-12-02 16:41:09 -0700

    added commenting, deleting posts, and some other things

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,13 +1,15 @@
-import mongoose from 'mongoose'
-import { AccountSchema } from '../models/Account'
-import { PostSchema } from '../models/Post';
-import { ValueSchema } from '../models/Value'
+import mongoose from "mongoose";
+import { AccountSchema } from "../models/Account";
+import { CommentSchema } from "../models/Comment.js";
+import { PostSchema } from "../models/Post";
+import { ValueSchema } from "../models/Value";
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
-  Account = mongoose.model('Account', AccountSchema);
-  Posts = mongoose.model('Post', PostSchema);
+  Values = mongoose.model("Value", ValueSchema);
+  Account = mongoose.model("Account", AccountSchema);
+  Posts = mongoose.model("Post", PostSchema);
 
+  Comments = mongoose.model("Comment", CommentSchema);
 }
 
-export const dbContext = new DbContext()
+export const dbContext = new DbContext();
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,19 +1,45 @@
-import { dbContext } from "../db/DbContext.js"
-
+// import { logger } from "../../ser/app/Utils/Logger.js";
+import { dbContext } from "../db/DbContext.js";
+import { BadRequest, Forbidden } from "../utils/Errors.js";
+import { logger } from "../utils/Logger.js";
 
 class PostsService {
+  async createPost(body) {
+    const post = await dbContext.Posts.create(body);
+    await post.populate("poster", "name");
+    return post;
+  }
 
-    async createPost(body) {
-        const post = await dbContext.Posts.create(body)
-        await post.populate('poster', 'name')
-        return post
-    }
+  async getAllPosts() {
+    const posts = await dbContext.Posts.find().populate(
+      "poster",
+      "name picture"
+    );
+    return posts;
+  }
+  async getPostDetails(postId) {
+    const post = await dbContext.Posts.findById(postId).populate(
+      "poster",
+      "name picture"
+    );
+    await post.populate("comments");
+    return post;
+  }
 
-    async getAllPosts(query) {
-        const posts = await dbContext.Posts.find(query).populate('')
-        return posts
+  async deletePost(userId, postId) {
+    const post = await dbContext.Posts.findById(postId);
+    logger.log(post.id);
+    logger.log(userId);
+    if (post) {
+      if (userId == post.posterId) {
+        return await dbContext.Posts.findByIdAndDelete(postId);
+      } else {
+        throw new Forbidden("YOU ARE NOT THE PERSON THAT MADE THAT POST");
+      }
+    } else {
+      throw new BadRequest("Cant find a post with that ID");
     }
+  }
 }
 
-
-export const postsService = new PostsService()
+export const postsService = new PostsService();
